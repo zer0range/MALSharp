14d996f92d4ac1a6497820f926afda2bc142f2f5
zer0range <orange.tenshi@gmail.com>
2018-05-12 12:33:28 +0200

    Generic  HTTP GET  func was added with XML deser.

--- MALSharp/Service/MALServices.cs
+++ MALSharp/Service/MALServices.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace MALSharp.Service
 {
     class MALServices
     {
+        private async Task<T> GetAsync<T>(Uri uri)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(uri);
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                var xml = await response.Content.ReadAsStreamAsync();
+                T result = (T)serializer.Deserialize(xml);
+                return result;
+            }
+        }
     }
 }
